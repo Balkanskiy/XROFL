0fd9afc0ea53a933419ff484c6d8758346801390
Alex <balkanskiy.alexey@gmail.com>
2022-01-28 18:26:49 +0100

    Add some content for root

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { VRCanvas, DefaultXRControllers } from '@react-three/xr'
 function App() {
   return (
     <div className="App">
+      hi
       <VRCanvas>
         {/* All your regular react-three-fiber elements go here */}
         <DefaultXRControllers />
