8658f243da6b42326238d5123d8a2e7adf4f2ed5
Alex <balkanskiy.alexey@gmail.com>
2022-01-28 21:52:58 +0100

    Updates

---
+++ precache-manifest.e76eb62304e15331ad3471a7a13c13e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac81cff351709577f0b78a5877292a2e",
+    "url": "/XROFL/index.html"
+  },
+  {
+    "revision": "641e51a096d90aa4055a",
+    "url": "/XROFL/static/css/main.f978bc51.chunk.css"
+  },
+  {
+    "revision": "e66b9b8f69b7d23b8957",
+    "url": "/XROFL/static/js/2.d8d990f3.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/XROFL/static/js/2.d8d990f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641e51a096d90aa4055a",
+    "url": "/XROFL/static/js/main.754f6052.chunk.js"
+  },
+  {
+    "revision": "ea383f1d61fbf7fd6d89",
+    "url": "/XROFL/static/js/runtime-main.157d8170.js"
+  }
+]);
